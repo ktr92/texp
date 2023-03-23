7ca87dd68c4ccbd8b6c82d7b0edfdc02797c57bc
ktr92 <kudashevtim@gmail.com>
2023-03-23 11:40:45 +0300

    header and mainblock

---
+++ src/js/default.js
@@ -0,0 +1,70 @@
+function initFE() {
+  closeByClickOutside('.headersearch', '[data-toggleclick="headersearch"]')
+}
+
+
+
+function closeByClickOutside(element, button, callback) {
+  $(document).click(function (event) {
+    if (!$(event.target).closest(`${element},${button}`).length) {
+      $(button).removeClass('active')
+      $(element).removeClass('active')
+    }
+  });
+
+  $(document).keyup(function (e) {
+    if (e.key === "Escape") { // escape key maps to keycode `27`
+      $(button).removeClass('active')
+      $(element).removeClass('active')
+    }
+  });
+
+  if (callback instanceof Function) { callback(); }
+}
+
+$(document).ready(function () {
+  $('[data-toggleclick]').on('click', function (e) {
+    $(this).toggleClass('active')
+    e.preventDefault()
+    let dropdown = $(this).data('toggleclick')
+    $('[data-toggle].active').not($(`[data-toggle=${dropdown}]`)).removeClass('active')
+    $('[data-toggleclick].active').not($(`[data-toggleclick=${dropdown}]`)).removeClass('active')
+    $(`[data-toggle=${dropdown}]`).toggleClass('active')
+    $(`[data-toggleactive=${dropdown}]`).toggleClass('active')
+  })
+
+  $('[data-toggleclickset]').on('click', function (e) {
+    $(this).toggleClass('active')
+    e.preventDefault()
+    let dropdown = $(this).data('toggleclickset')
+    let wrapper = $(this).closest(`[data-toggleitem]`)
+    $('[data-toggleitem].active').not(wrapper).removeClass('active')
+    $('[data-toggle].active').not(wrapper.find(`[data-toggle=${dropdown}]`)).removeClass('active')
+    $('[data-toggleclickset].active').not(wrapper.find(`[data-toggleclickset=${dropdown}]`)).removeClass('active')
+    wrapper.addClass('active')
+    wrapper.find(`[data-toggle=${dropdown}]`).toggleClass('active')
+    $(`[data-toggleactive=${dropdown}]`).toggleClass('active')
+  })
+
+
+  $('[data-menutoggle]').on('click', function (e) {
+    e.preventDefault()
+    let menu = $(this).data('menutoggle')
+    $(`[data-menu=${menu}]`).toggleClass('active')
+    $(this).toggleClass('active')
+    $('.jsbackdrop').toggleClass('active')
+    $('body').toggleClass('block')
+
+  })
+  $('.jsbackdrop').on('click', function (e) {
+    $(this).removeClass('active')
+    $('[data-menu]').removeClass('active')
+    $('[data-menutoggle]').removeClass('active')
+    $('body').removeClass('block')
+
+  })
+})
+
+window.addEventListener('load', function () {
+  initFE()
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,111 +1,70 @@
-
-$(document).ready(function() { 
-  $("input[type=tel]").mask("7 (999) 999-99-99");
-
-  $(function() {
-	
-	$(".accordion__title").on("click", function(e) {
-
-		e.preventDefault();
-		var $this = $(this);
-
-		if (!$this.hasClass("accordion-active")) {
-			$(".accordion__content").slideUp(400);
-			$(".accordion__title").removeClass("accordion-active");
-			$('.accordion__arrow').removeClass('accordion__rotate');
-		}
-
-		$this.toggleClass("accordion-active");
-		$this.next().slideToggle();
-		$('.accordion__arrow',this).toggleClass('accordion__rotate');
-	});
-	//END
-	
-});
-
-$("a.scrollTo").click(function (e) {
-	e.preventDefault()
-	var destination = $($(this).attr("href")).offset().top - 10;
-	$("html:not(:animated),body:not(:animated)").animate({
-		scrollTop: destination
-	}, 600);
-	$('.mainmenu').removeClass('active')
-	$('.menubtn').removeClass('active')
-	return false;
-});
-
-
-window.addEventListener('scroll', (event) => {
-	scroll = window.scrollY
-	if (scroll >= 1200) {
-		document.getElementById("gototop").classList.add('active')
-	} else {
-		document.getElementById("gototop").classList.remove('active')
-	}
-
-});
-
-$('button.menubtn').on('click', function(e) {
-	e.preventDefault()
-	$(this).toggleClass('active')
-	$('.mainmenu').toggleClass('active')
-})
-
-function closeByClickOutside(element, button) {
-  $(document).click(function(event) {
-      if (!$(event.target).closest(`${element},${button}`).length) {
-          $(button).removeClass('active')
-          $(element).removeClass('active')
-      }
-  });
-  
-  $(document).keyup(function(e) {
-      if (e.key === "Escape") { // escape key maps to keycode `27`
-          $(button).removeClass('active')
-          $(element).removeClass('active')
-      }
-  });
+function initFE() {
+	closeByClickOutside('.headersearch', '[data-toggleclick="headersearch"]')
 }
 
-closeByClickOutside('.mainmenu', '.menubtn')
 
-var wow = new WOW();
-wow.init();
 
-$(function() {
-  return $(".modal").on("show.bs.modal", function() {
-    var curModal;
-    curModal = this;
-    $(".modal").each(function() {
-      if (this !== curModal) {
-        $(this).modal("hide");
-      }
-    });
-  });
-});
+function closeByClickOutside(element, button, callback) {
+	$(document).click(function (event) {
+		if (!$(event.target).closest(`${element},${button}`).length) {
+			$(button).removeClass('active')
+			$(element).removeClass('active')
+		}
+	});
 
+	$(document).keyup(function (e) {
+		if (e.key === "Escape") { // escape key maps to keycode `27`
+			$(button).removeClass('active')
+			$(element).removeClass('active')
+		}
+	});
+
+	if (callback instanceof Function) { callback(); }
+}
+
+$(document).ready(function () {
+	$('[data-toggleclick]').on('click', function (e) {
+		$(this).toggleClass('active')
+		e.preventDefault()
+		let dropdown = $(this).data('toggleclick')
+		$('[data-toggle].active').not($(`[data-toggle=${dropdown}]`)).removeClass('active')
+		$('[data-toggleclick].active').not($(`[data-toggleclick=${dropdown}]`)).removeClass('active')
+		$(`[data-toggle=${dropdown}]`).toggleClass('active')
+		$(`[data-toggleactive=${dropdown}]`).toggleClass('active')
+	})
+
+	$('[data-toggleclickset]').on('click', function (e) {
+		$(this).toggleClass('active')
+		e.preventDefault()
+		let dropdown = $(this).data('toggleclickset')
+		let wrapper = $(this).closest(`[data-toggleitem]`)
+		$('[data-toggleitem].active').not(wrapper).removeClass('active')
+		$('[data-toggle].active').not(wrapper.find(`[data-toggle=${dropdown}]`)).removeClass('active')
+		$('[data-toggleclickset].active').not(wrapper.find(`[data-toggleclickset=${dropdown}]`)).removeClass('active')
+		wrapper.addClass('active')
+		wrapper.find(`[data-toggle=${dropdown}]`).toggleClass('active')
+		$(`[data-toggleactive=${dropdown}]`).toggleClass('active')
+	})
+
+
+	$('[data-menutoggle]').on('click', function (e) {
+		e.preventDefault()
+		let menu = $(this).data('menutoggle')
+		$(`[data-menu=${menu}]`).toggleClass('active')
+		$(this).toggleClass('active')
+		$('.jsbackdrop').toggleClass('active')
+		$('body').toggleClass('block')
+
+	})
+	$('.jsbackdrop').on('click', function (e) {
+		$(this).removeClass('active')
+		$('[data-menu]').removeClass('active')
+		$('[data-menutoggle]').removeClass('active')
+		$('body').removeClass('block')
+
+	})
 })
 
-
-$(document).ready(function (){
-
-	YaMapsShown = false; 
-
-	function showYaMaps(){
-		var script   = document.createElement("script");
-		script.type  = "text/javascript";
-		script.src   = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A29effecca8741f8c7ba884726c458ad6344e6e30dc1bc81265e789bd75832c6e&amp;width=100%25&amp;height=720&amp;lang=ru_RU&amp;scroll=true";
-		document.getElementById("YaMaps").appendChild(script);
-	 }
-
-	 
-	$(window).scroll(function() {
-		 if (!YaMapsShown){
-			if($(window).scrollTop() + $(window).height() > $(document).height() - 700) {      
-			 showYaMaps();
-			 YaMapsShown = true;
-			}
-		 }
-	});
-	
- });
+window.addEventListener('load', function () {
+	initFE()
+});
